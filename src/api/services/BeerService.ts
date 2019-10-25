c9322e13760cf44f8e927c1ca26b1d7e4aaf53e2
Caroline <caroline@bluerosedev.com>
2019-10-25 15:26:20 +0100

    fix: Remove redundant try catch blocks from BeerService methods

--- src/api/services/BeerService.ts
+++ src/api/services/BeerService.ts
@@ -11,54 +11,42 @@ export class BeerService implements APIService<Beer> {
 
     async find(page: number = 1, pageSize: number = 20, ids?: number[]): Promise<Beer[]> {
 
-        try {
+        let url = `${apiBaseUrl}/beers/?page=${page}&per_page=${pageSize}`;
 
-            let url = `${apiBaseUrl}/beers/?page=${page}&per_page=${pageSize}`;
-
-            if (ids && ids.length) {
-                url += `&ids=${ids.join('|')}`;
-            }
-
-            const response: Response = await fetch(url);
-
-            if (!(response && response.ok)) {
-                ErrorService.handleHttpError(response.status);
-            }
-
-            const json = await response.json();
-
-            if (!Array.isArray(json)) {
-                throw new Error(`Unexpected return type: ${typeof json}`);
-            }
-
-            return json.map((item: any) => new Beer(item));
-
-        } catch (err) {
-            throw err;
+        if (ids && ids.length) {
+            url += `&ids=${ids.join('|')}`;
         }
+
+        const response: Response = await fetch(url);
+
+        if (!(response && response.ok)) {
+            ErrorService.handleHttpError(response.status);
+        }
+
+        const json = await response.json();
+
+        if (!Array.isArray(json)) {
+            throw new Error(`Unexpected return type: ${typeof json}`);
+        }
+
+        return json.map((item: any) => new Beer(item));
     }
 
     async findById(id: number): Promise<Beer | undefined> {
 
-        try {
+        const response: Response = await fetch(`${apiBaseUrl}/beers/${id}`);
 
-            const response: Response = await fetch(`${apiBaseUrl}/beers/${id}`);
-
-            if (!(response && response.ok)) {
-                ErrorService.handleHttpError(response.status);
-            }
-
-            const json = await response.json();
-
-            if (!Array.isArray(json) && json.length === 1) {
-                throw new Error(`Unexpected return type: ${typeof json}`);
-            }
-
-            return new Beer(json[0]);
-
-        } catch (err) {
-            throw err;
+        if (!(response && response.ok)) {
+            ErrorService.handleHttpError(response.status);
         }
+
+        const json = await response.json();
+
+        if (!Array.isArray(json) && json.length === 1) {
+            throw new Error(`Unexpected return type: ${typeof json}`);
+        }
+
+        return new Beer(json[0]);
     }
 }
 

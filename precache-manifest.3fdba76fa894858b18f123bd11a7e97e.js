a15a43a52b3c118a3ace5e90b9085b5e6fe42553
Caroline <caroline@bluerosedev.com>
2019-10-29 11:56:50 +0000

    Updates

---
+++ precache-manifest.3fdba76fa894858b18f123bd11a7e97e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d1b7444334154e1f45f369c155c9b9",
+    "url": "/beer-app/index.html"
+  },
+  {
+    "revision": "0a85a71db318759611ba",
+    "url": "/beer-app/static/css/main.9b0bc1ad.chunk.css"
+  },
+  {
+    "revision": "90fe38f1acc77111ec45",
+    "url": "/beer-app/static/js/2.ecd793df.chunk.js"
+  },
+  {
+    "revision": "0a85a71db318759611ba",
+    "url": "/beer-app/static/js/main.16b9c50b.chunk.js"
+  },
+  {
+    "revision": "abbee4ba6a29869eeab1",
+    "url": "/beer-app/static/js/runtime-main.adc44b80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad17c7baa05aa7b873394b8541556200.js"
+  "/beer-app/precache-manifest.3fdba76fa894858b18f123bd11a7e97e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beer-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
